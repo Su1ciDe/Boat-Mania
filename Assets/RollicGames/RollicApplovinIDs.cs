b88c81e3ccaf34aca2441cc9ee54e5a367109275
FiberGamesArsiv <info@fibergames.com.tr>
2024-09-26 19:28:39 +0300

    game keys-boat speed

--- Assets/RollicGames/RollicApplovinIDs.cs
+++ Assets/RollicGames/RollicApplovinIDs.cs
@@ -2,14 +2,14 @@ namespace RollicGames.Advertisements
 {
 	public class RollicApplovinIDs
 	{
-		public static string AppKey = "[TEMP_GAMEKIT_AppKey]";
-		public static string BannerAdUnitIos = "[TEMP_GAMEKIT_BannerAdUnitIos]";
-		public static string InterstitialAdUnitIos = "[TEMP_GAMEKIT_InterstitialAdUnitIos]";
-		public static string RewardedAdUnitIos = "[TEMP_GAMEKIT_RewardedAdUnitIos]";
-		public static string BannerAdUnitAndroid = "[TEMP_GAMEKIT_BannerAdUnitAndroid]";
-		public static string InterstitialAdUnitAndroid = "[TEMP_GAMEKIT_InterstitialAdUnitAndroid]";
-		public static string RewardedAdUnitAndroid = "[TEMP_GAMEKIT_RewardedAdUnitAndroid]";
-		public static string GoogleIosId = "[TEMP_GAMEKIT_GoogleAppIdIos]";
-		public static string GoogleAndroidId = "[TEMP_GAMEKIT_GoogleAppIdAndroid]";
+		public static string AppKey = "HXfHkhyBTH16scCO01rE3sKd5GHRl9bNlMnsETWk9y2R_SsH6y6m5N3HrWutctNv04y0EIdil5zJu0FMT47KOQ";
+		public static string BannerAdUnitIos = "2e489937ce5657c2";
+		public static string InterstitialAdUnitIos = "6c1afa57666a8147";
+		public static string RewardedAdUnitIos = "98a9a9cd6b847670";
+		public static string BannerAdUnitAndroid = "a1d178f808398bb4";
+		public static string InterstitialAdUnitAndroid = "519025ea37acc122";
+		public static string RewardedAdUnitAndroid = "3b3920870b52de0c";
+		public static string GoogleIosId = "";
+		public static string GoogleAndroidId = "";
 	}
 }
